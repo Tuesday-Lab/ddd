aa5eaf20caf374f67ea15ca82817bc7563a784ea
Alice <with.alice.lee@gmail.com>
2020-10-21 22:50:32 +0900

    feat: 이벤트 생성 구현

--- event/models.py
+++ event/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from pydantic import BaseModel, validator
 
 from base.db.mixin import TimestampMixin
+from base.extension import ExtendedEnum
+
+
+class Currency(str, ExtendedEnum):
+    KRW = "KRW"
+    USD = "USD"
+
+
+class Money(BaseModel):
+    amount: int
+    currency: Currency
+
+    @validator('amount')
+    def amount_must_be_positive(cls, v):
+        if v < 0:
+            raise ValueError("amount should be grater then 0")
+        return v
 
 
 class Event(TimestampMixin, models.Model):
@@ -10,7 +28,7 @@ class Event(TimestampMixin, models.Model):
     id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=255)
     # host_user_id = models.ForeignKey(User) # TODO Custom Event User
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
 
     kind = models.CharField(max_length=10)  # Todo EventKind
     start_time = models.DateTimeField(null=True)

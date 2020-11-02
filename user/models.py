2c83a2d40e6230df87844a05ed3696f56a8e6615
sunhooo <sssunho.lee@gmail.com>
2020-11-03 02:56:59 +0900

    feat: JWT 토큰 발급
    rest_framework JWT 사용으로 Manager 활용 필요하여
    Repo 레이어 제거함

--- user/models.py
+++ user/models.py
@@ -1,5 +1,35 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+from base.exceptions import ConflictResource
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+# Custom User 모델을 사용하다 보니 매니저를 활용할 수 밖에 없어서
+# 레포 레이어 삭제..
+# -> 매니저&모델 사용하는걸 레포레이어로 감쌀까 했으나
+# 어떤건 매니저 어떤건 레포 등으로 분리될 것 같아 안나눔
+class UserManager(BaseUserManager):
+    def create_user(self, email: str, password: str, name: str):
+        if self.is_exist(email):
+            raise ConflictResource(detail="duplicate email")
+
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def is_exist(self, email):
+        return UserModel.objects.filter(email=email).exists()
 
 
 class UserModel(AbstractBaseUser):
@@ -7,11 +37,7 @@ class UserModel(AbstractBaseUser):
         max_length=255,
         unique=True,
     )
-    name = models.CharField(
-        max_length=20,
-        null=False,
-        unique=False
-    )
+    name = models.CharField(max_length=20, null=False, unique=False)
     password = models.CharField(max_length=128)
 
     is_active = models.BooleanField(default=True)
@@ -19,9 +45,11 @@ class UserModel(AbstractBaseUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
 
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = ["name"]
+
+    objects = UserManager()
 
     @property
     def is_staff(self):
@@ -30,5 +58,4 @@ class UserModel(AbstractBaseUser):
         return self.is_admin
 
     class Meta:
-        db_table = 'user'
-
+        db_table = "user"
--- user/services.py
+++ user/services.py
@@ -1,19 +1,22 @@
 from django.db import transaction
 
-from base.exceptions import ConflictResource
-from user.repositories import UserRepo
+from user.models import UserModel
+
+
+# from user.repositories import UserRepo
 
 
 class UserService:
-    def __init__(self):
-        self.user_repo = UserRepo()
+    # def __init__(self):
+    #     self.user_repo = UserRepo()
 
     @transaction.atomic
     def signup(self, email: str, name: str, password: str):
         # 심플한 레포일 때도 서비스 레이어에서 트랜잭션 관리하는게 좋을까?
-        user = self.user_repo.create_user(email, name, password)
+
+        # user = self.user_repo.create_user(email, name, password)
+        user = UserModel.objects.create_user(email, password, name)
         return user
 
 
 service = UserService()
-

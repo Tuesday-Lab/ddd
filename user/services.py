1b025256363116513741efdb0b2b228488ef445c
Sunho <sunho@wantedlab.com>
2020-11-11 01:00:03 +0900

    refactor: repo 사용 안함

--- user/services.py
+++ user/services.py
@@ -3,16 +3,9 @@ from django.db import transaction
 from user.models import UserModel
 
 
-# from user.repositories import UserRepo
-
-
 class UserService:
-    # def __init__(self):
-    #     self.user_repo = UserRepo()
-
     @transaction.atomic
     def signup(self, email: str, name: str, password: str):
-        # user = self.user_repo.create_user(email, name, password)
         user = UserModel.objects.create_user(email, password, name)
         return user
 
